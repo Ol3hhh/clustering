0c3b2cd0407d9145324b0f066177cebcf5a05dd0
Ol3hhh <olehmaru@gmail.com>
2024-10-26 18:36:28 +0200

    comments update

--- Py/GUI.py
+++ Py/GUI.py
@@ -3,13 +3,14 @@ from tkinter import ttk
 from clustering import clustering, points, categories, glasses, alcoholic, tags, ingredients
 
 
-# Graphical interface using tkinter
+# GUI analysis of cocktails using tkinter
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("Kmeans Analysis")
+        self.title("K-means Analysis")
         self.geometry("800x600")
 
+        # Create main frame with a canvas and vertical scrollbar
         self.main_frame = tk.Frame(self)
         self.main_frame.pack(fill=tk.BOTH, expand=1)
 
@@ -22,27 +23,33 @@ class App(tk.Tk):
         self.canvas.configure(yscrollcommand=self.scrollbar.set)
         self.canvas.bind('<Configure>', lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
 
+        # Create a frame inside the canvas for content
         self.second_frame = tk.Frame(self.canvas)
-
         self.canvas.create_window((0, 0), window=self.second_frame, anchor="nw")
 
+        # Label and ComboBox for selecting a cocktail
         self.label = tk.Label(self.second_frame, text="Choose a cocktail from the list")
         self.label.pack(pady=10)
 
+        # Populate ComboBox with cocktail names
         self.combo = ttk.Combobox(self.second_frame, values=[point[0] for point in points])
         self.combo.pack(pady=10)
         self.combo.bind("<<ComboboxSelected>>", self.show_similar)
 
+        # Label to display similar cocktails
         self.result_label = tk.Label(self.second_frame, text="", justify=tk.LEFT)
         self.result_label.pack(pady=10)
 
     def show_similar(self, event):
         selected_cocktail = self.combo.get()
+
+        # Find the selected cocktail and retrieve similar cocktails
         for point in points:
             if point[0] == selected_cocktail:
                 similar = clustering.compare(point)
                 break
 
+        # Format and display information for similar cocktails
         similar_info = []
         for cocktail in similar:
             category = list(categories.keys())[list(categories.values()).index(cocktail[1])]
--- Py/main.py
+++ Py/main.py
@@ -1,5 +1,6 @@
 from GUI import App
 
+# Main entry point for the application
 if __name__ == "__main__":
     app = App()
     app.mainloop()
