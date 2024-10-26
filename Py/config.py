478ba2140f17c43db93abbfe1a61d3d8053ad6ce
Ol3hhh <olehmaru@gmail.com>
2024-10-26 12:22:27 +0200

    files deleted

--- Py/config.py
+++
@@ -1,5 +0,0 @@
-json_file_path = 'D:/My/Pwr/SolVro/recrutation/Source/cocktail_dataset.json'
-txt_file_path = r'D:\My\Pwr\SolVro\recrutation\Source\cocktail_dataset.txt'
-id_path = 'D:/My/Pwr/SolVro/recrutation/Source/id.txt'
-category_path = 'D:/My/Pwr/SolVro/recrutation/Source/category.txt'
-glass_path = 'D:/My/Pwr/SolVro/recrutation/Source/glass.txt'
